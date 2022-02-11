ec48ca3b850e51a6c6344e926e9f04b05faa0f67
Ahmed Sabbir <theahmedsabbir@gmail.com>
2022-02-11 20:32:49 +0600

    update

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -1,8 +1,8 @@
 
 <footer class="br-footer">
   <div class="footer-left">
-    <div class="mg-b-2">Copyright &copy; 2021. <a href="" target="_blank">Tyreguru</a>. All Rights Reserved.</div>
-    <div>Design & developed by <a href="https://emicontech.com/" target="_blank">Emicon Technology</a>.</div>
+    <div class="mg-b-2">Copyright &copy; 2022. <a href="" target="_blank">CTT</a>. All Rights Reserved.</div>
+    <div>Design & developed by <a href="https://wish-bd.com/" target="_blank">WISH</a>.</div>
   </div>
   <div class="footer-right d-flex align-items-center">
     <span class="tx-uppercase mg-r-10">Share:</span>
